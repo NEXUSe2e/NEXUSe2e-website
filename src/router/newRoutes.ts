2c2031ae0d629995a4a46fc699066fd45b2a662a
Lukas Berns <lukas.berns@evolvsys.com>
2024-06-25 10:03:29 +0200

    Simplified routes

--- src/router/newRoutes.ts
+++ src/router/newRoutes.ts
@@ -1,33 +1,27 @@
 export default [
   {
     path: '',
-    name: 'newOverview',
-    component: () => import(/* webpackChunkName: "NewOverviewPage" */ '@/pages/new/index.vue')
+    component: () => import('@/pages/new/index.vue')
   },
   {
     path: 'docs',
-    name: 'docs',
-    component: () => import(/* webpackChunkName: "Documentation" */ '@/pages/new/docs/index.vue')
-  }, 
+    component: () => import('@/pages/new/docs/index.vue')
+  },
   {
     path: 'docs/overview',
-    name: 'overview',
-    component: () => import(/* webpackChunkName: "Overview" */ '@/pages/new/docs/overview/index.vue')
-  }, 
+    component: () => import('@/pages/new/docs/overview/index.vue')
+  },
   {
     path: 'docs/dictionary',
-    name: 'dictionary',
-    component: () => import(/* webpackChunkName: "Overview" */ '@/pages/new/docs/dictionary/index.vue')
-  }, 
-  
+    component: () => import('@/pages/new/docs/dictionary/index.vue')
+  },
+
   {
     path: 'contact',
-    name: 'contact',
-    component: () => import(/* webpackChunkName: "ContactPage" */ '@/pages/new/contact/index.vue')
+    component: () => import('@/pages/new/contact/index.vue')
   },
   {
     path: 'imprint',
-    name: 'imprint',
-    component: () => import(/* webpackChunkName: "ImprintPage" */ '@/pages/new/imprint/index.vue')
+    component: () => import('@/pages/new/imprint/index.vue')
   }
 ]
