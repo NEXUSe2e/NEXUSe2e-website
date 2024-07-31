1803d9b9edb00bae9e1ca133ff4d7dabc7fa5297
Guido Esch <guido.esch@evolvsys.com>
2024-07-31 18:39:51 +0200

    some content fixed.

--- src/pages/new/docs/components/services/index.vue
+++ src/pages/new/docs/components/services/index.vue
@@ -6,6 +6,11 @@
         src="@/assets/new_service1.png"
       />
       <div v-html="markdown2" />
+      <v-img
+        src="@/assets/new_service2.png"
+      />
+      <div v-html="markdown3" />
+
     </v-col>
   </v-row>
 </template>
@@ -23,16 +28,15 @@ const markdown1 = marked(`
 
   After selecting the type (#1), the component selection (#2) is updated. The selected component is also removed as soon as the type is changed.
 
-
-
 `)
 const markdown2 = marked(`
 
   Depending on the type, the pipeline selection at the bottom gets visible or not. All inflow and outflow services have multiple pipelines available to work with. The Support Services are only referenced by other services and have no pipelines.
-  As soon as you pick a component, the parameter options for the selected component are shown. While every component defines their own required parameters, all services require at least the name.
+  As soon as you pick a component, the parameter options for the selected component are shown. While every component defines their own required parameters, all services require at least the name. As soon as the service is saved, its shown in the overview list.
 
+`)
 
-
-
+const markdown3 = marked(`
+  The Overview list shows all services which can be enabled or disabled at any time. When clicking on one service (#1), the common list view right side drawer will appear and shown the service configuration. For manual runs,the service can be triggered by clicking the play button (#2). The action bar in the drawer also contains a edit button (#3) and a close button (#4).
 `)
 </script>
