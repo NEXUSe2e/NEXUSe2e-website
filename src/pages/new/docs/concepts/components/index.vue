9ffa3a498db85535a1a696102e07e694afb4e063
Guido Esch <102174945+esc-gesch@users.noreply.github.com>
2024-07-31 17:10:14 +0200

    Update index.vue
    Suggestion for styling :)

--- src/pages/new/docs/concepts/components/index.vue
+++ src/pages/new/docs/concepts/components/index.vue
@@ -22,23 +22,23 @@ All components are automatically scanned and available after startup. Manuall re
 `)
 
 const markdown2 = marked(`
-## 1 Name
+### #1 Name
 Shows the name of the component. The name is unique and used to reference the component in service or pipelet configurations.
 
-## 2 Type
+### #2 Type
 Type shows the type of the component. This is either
 - Pipelet, used in pipelines
 - Inflow Service, see [Inflow and Outflow](#/new/docs/concepts/flows)
 - Outflow Service, see [Inflow and Outflow](#/new/docs/concepts/flows)
 - Support Service, a special service that is used to configure and implement reusable functionality. May be used in other services or pipelets.
 
-## 3 Class
+### #3 Class
 The fully qualified class name of the component.
 
-## 4 Description
+### #4 Description
 The description shows a short description what the component does.
 
-## 5 Filter
+### #5 Filter
 The filter can be used to filter the list of components. The filter is applied to the name or type of the component.
 `)
 
