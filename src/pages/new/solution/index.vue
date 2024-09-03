26b2ced8272257f398327a0740793228c4d17d40
Sven Barden <sven.barden@evolvsys.com>
2024-09-03 10:57:20 +0200

    fixed typos

--- src/pages/new/solution/index.vue
+++ src/pages/new/solution/index.vue
@@ -28,7 +28,7 @@ const markdown2 = marked(`
 ## Even for smaller applications.
 NEXUSe2e is a toolbox that is tailored to the needs of our customers. Every process and every connection has its own requirements, which need to be discussed in detail. NEXUSe2e is software that can transmit and convert data securely and reliably.
 
-✓ The receipt of messages can be confirmed by additional technical confirmation messages.<br/>
+✓ The receipt of messages can be confirmed by additional technical confirmation messages<br/>
 ✓ Generalized messaging workflow, independent of protocols, ready to be extended to handle different communication protocol<br/>
 ✓ Individual settings for maximum number of retries and interval between resends for all connections<br/>
 ✓ Transaction logs reflect the status of the message confirmation<br/>
