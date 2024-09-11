88f1c9dd52394b545ff56176a7c1733d5d9021e8
Kolja Barkam <kolja.barkam@evolvsys.com>
2024-09-11 16:21:48 +0200

    Add doc on how to configure the UI max file size

---
+++ src/pages/new/docs/configuration/allowed-file-size/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <div v-html="markdown1" />
+      <NEXUSe2eImage src="@/assets/max_file_size.png" max-width="306px"/>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+import NEXUSe2eImage from '@/components/nexus2eImage/NEXUSe2eImage.vue'
+
+import { marked } from 'marked'
+
+const markdown1 = marked(`
+# Allowed File Size
+---
+
+By default NEXUSe2e only allows a limited file size when uploading files in the UI (e.g. selecting a file to send using 'Manual Message" function).\\
+This does not effect the allowed size of payloads send by external partners or internal backend systems.\\
+The purpose of the limit is to protect against uploading files too large for the system to handle.
+
+When trying to upload a file is larger then the allowed limit, a "Couldn't upload file" error will be displayed.
+
+It is possible to configure this limit to allow uploading larger files, but should be done with care and might require more resources (memory).\\
+If not explicitly configured the default limit is currently 5 MB.
+
+The configuration is done in the NEXUSe2e application properties.\\
+For NEXUSe2e running in Tomcat the application properties are usually located in the application-default.yml in the Tomcat /conf directory.
+
+Locate the spring.servlet.multipart.max-file-size and spring.servlet.multipart.max-request-size properties or add them if missing and set them to the desired value, e.g. to 20 MB:
+`)
+</script>
