cb807099aa71e4a66caf3bc8140f7013b1ed9a5b
Lukas Berns <lukas.berns@evolvsys.com>
2026-03-25 10:41:55 +0100

    Updates

---
+++ assets/index-DYx6DGfb.js
@@ -0,0 +1,23 @@
+import{N as r}from"./NEXUSe2eImage-CAhcqOUd.js";import{d as e}from"./marked.esm-C0Rlrcoy.js";import{d as l,c,w as o,V as u,a as t,b as n,u as s,e as p,o as h}from"./index-C0PiF630.js";const d="/assets/light-dark-tYDlmkUc.png",m=["innerHTML"],w=["innerHTML"],x=l({__name:"index",setup(f){const a=e(`
+# Home
+
+---
+
+## The next generation of NEXUSe2e</h2>
+
+`),i=e(`
+
+## NEXUSe2e 10.0
+
+The next generation of NEXUSe2e is now available. While the 5.x version is still available and will stay as an open source solution for anyone to use, the new implementation of NEXUSe2e starts with version 10.
+\\
+The new NEXUSe2e implementation is a full rewrite to keep it separate from the open source solution. We stick with the convincing concepts but also redesigned quite a few models and concepts to make them future prove and more flexible or just simpler than before. The rewrite provides us with the opportunity to replace all aged software libraries and replace them with up to date technologies. Therefore we only support Java versions >= 21 and Tomcat >= 10.1. For security and compliance purposes we now support technologies like OAuth2 and secure storages like Azure Keyvault.
+\\
+Besides of all the technical improvements, we also changed to the license to a closed source model.
+\\
+Feel free to get in touch with us and we help you getting your new version going.
+
+### Your NEXUSe2e Team
+
+
+`);return(g,_)=>(h(),c(u,null,{default:o(()=>[t(p,{cols:"12"},{default:o(()=>[n("div",{innerHTML:s(a)},null,8,m),t(r,{"max-height":"550px",src:d}),n("div",{innerHTML:s(i)},null,8,w)]),_:1})]),_:1}))}});export{x as default};
--- assets/index-px_no6dU.js
+++
@@ -1,6 +0,0 @@
-import{m as o}from"./marked.esm-8ZxQ78w7.js";import{d as n,c as s,w as e,V as t,o as r,a as c,b as d,u as m,e as _}from"./index-DIN51Eex.js";const i=["innerHTML"],k=n({__name:"index",setup(l){const a=o(`
-  # Recommendations
-  ---
-
-
-`);return(u,f)=>(r(),s(t,null,{default:e(()=>[c(_,{cols:"12"},{default:e(()=>[d("div",{innerHTML:m(a)},null,8,i)]),_:1})]),_:1}))}});export{k as default};
